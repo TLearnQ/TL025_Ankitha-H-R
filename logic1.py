9c84b6d7ac8d0177c6dc7711d5d7fafe44a4d9f2
Ankitharavigowda <Ankithagowdaravi@gmail.com>
2025-12-06 10:55:34 +0530

    Update logic1.py

--- logic1.py
+++ logic1.py
@@ -1,12 +1,3 @@
-values = [45, 67, 89, 23, 56]
 
-for v in values:
-    if v >= 80:
-        label = "High"
-    elif v >= 50:
-        label = "Medium"
-    else:
-        label = "Low"
-    
-    print(v, "→", label)
+
     
