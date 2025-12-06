52247a7363e33dbee2ac229855215def1b05b65b
Ankitharavigowda <Ankithagowdaravi@gmail.com>
2025-12-06 10:56:00 +0530

    Update logic2.py

--- logic2.py
+++ logic2.py
@@ -1,13 +1,2 @@
-cpu = [10, 40, 65, 95]
 
-for usage in cpu:
-    if usage <= 20:
-        status = "Idle"
-    elif usage <= 60:
-        status = "Normal"
-    elif usage <= 80:
-        status = "Busy"
-    else:
-        status = "Overloaded"
 
-    print(usage, "→", status)
